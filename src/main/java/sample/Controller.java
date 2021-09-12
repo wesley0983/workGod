aae75e6557a3b650dcbdd264f075eff8b624b49e
wesley <wesley0983@yahoo.com.tw>
2021-09-13 01:30:20 +0800

    測試

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    void click(ActionEvent event) {
+        System.out.println("test--");
+    }
 }
